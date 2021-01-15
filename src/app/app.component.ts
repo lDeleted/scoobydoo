397bc14aaf3d5b954a382341f9e31d57e3e653c3
John023 <36700696+John023@users.noreply.github.com>
2021-01-15 09:47:29 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,12 @@
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-// export class AppComponent {
-//   title = 'scooby';
-// }
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+
   palabra = 'GRACIAS';
   palabraOculta = '';
   intentos = 0;
